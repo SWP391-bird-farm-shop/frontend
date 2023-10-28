a3fa820ac9849042a43bf99b2ad030b022cab3de
lama-try-code <dotranlam2003@gmail.com>
2023-10-29 01:22:13 +0700

    ADD CONTENT

--- src/page/BlogContentPage.jsx
+++ src/page/BlogContentPage.jsx
@@ -1,37 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import RelatedPost from "../components/related-post/RelatedPost";
 import "./BlogContentPage.css";
+import { useParams } from "react-router-dom";
+import api from "../components/utils/requestAPI";
+import { useEffect } from "react";
 
 const BlogContentPage = () => {
-    const blogs = [
-        {
-            id: 1,
-            title: "The Importance of Proper Bird Cage Size",
-            date: "October 1, 2023",
-            author: "John Smith",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam...",
-            image: "sao.jpg",
-        },
-        // Add more blogs here
-    ];
+
+    const { blogId } = useParams();
+
+    const [blog, setBlog] = useState(null);
+
+    const fetchData = async () => {
+        const url = '/api/Blog/get-blog-by-id';
+        const data = {
+            blogID: blogId
+        }
+        try {
+            const response = await api.post(url, data);
+            console.log(response.data);
+            setBlog(response.data);
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const formatContent = (content) => {
+        const paragraphs = content?.split("\n\n");
+        return paragraphs?.map((paragraph, index) => (
+            <p key={index} className="blog-content-section-content">
+                {paragraph.split("\n").map((line, lineIndex) => (
+                    <React.Fragment key={lineIndex}>
+                        {line}
+                        <br />
+                    </React.Fragment>
+                ))}
+            </p>
+        ));
+    };
+
 
     return (
         <div className="blog-content-page">
-            {blogs.map((blog) => (
-                <div key={blog.id} className="blog-content-item">
-                    <div className="blog-content-item-information">
-                        <h2 className="blog-content-title">{blog.title}</h2>
-                        <p className="blog-content-meta">
-                            <span className="blog-content-date">{blog.date}</span> . <span className="blog-content-author">bởi {blog.author}</span>
-                        </p>
-                    </div>
-                    <div className="blog-content-section">
-                        <img className="blog-content-section-image" src={blog.image} alt="blog-content-image" />
-                        <h3 className="blog-content-section-heading">Hello how are you today</h3>
-                        <p className="blog-content-section-content">{blog.content}</p>
-                    </div>
+            <div key={blog?.blogId} className="blog-content-item">
+                <div className="blog-content-item-information">
+                    <h2 className="blog-content-title">{blog?.blogTitle}</h2>
+                    <p className="blog-content-meta">
+                        <span className="blog-content-date">{blog?.createAt}</span> . <span className="blog-content-author">bởi {blog?.user.fullName}</span>
+                    </p>
                 </div>
-            ))}
+                <div className="blog-content-section">
+                    <img className="blog-content-section-image" src={blog?.image[0].imageUrl} alt="blog-content-image" />
+                    <h3 className="blog-content-section-heading">{blog?.blogTitle}</h3>
+                    {formatContent(blog?.blogContent)}
+                </div>
+            </div>
             <RelatedPost />
         </div>
     );
