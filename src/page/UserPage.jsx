7025efa21876eff89d55d16c84dbbb8ba5b00366
Lam Do <84032766+lama-try-code@users.noreply.github.com>
2023-11-07 14:20:50 +0700

    Update UserPage.jsx

--- src/page/UserPage.jsx
+++ src/page/UserPage.jsx
@@ -4,7 +4,6 @@ import Article from '../components/article/Article'
 import Slider from '../components/slider/Slider'
 
 import 'bootstrap/dist/css/bootstrap.css';
-import { Button, Modal } from 'react-bootstrap';
 import { useState } from 'react';
 
 function UserPage() {
