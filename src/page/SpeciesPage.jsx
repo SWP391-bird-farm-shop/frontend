2f57c48a4ad6f3c68efbca9e3c66159546458d32
Kaminich <caominhnguyen03@gmail.com>
2023-11-10 12:29:04 +0700

    add api to SpeciesPage

--- src/page/SpeciesPage.jsx
+++ src/page/SpeciesPage.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import About from '../components/about/About';
 import Carousel from '../components/carousel/Carousel';
-import BirdList from '../components/bird-list/BirdList';
+import CageList from '../components/cage-list/CageList'
 import './SpeciesPage.css';
 
 const SpeciesPage = () => {
@@ -10,11 +10,11 @@ const SpeciesPage = () => {
             <About />
             <div className="bird-of-species-section">
                 <h3>Lồng phù hợp với chim</h3>
-                <BirdList />
+                <CageList />
             </div>
             <div className="different-species-carousel">
                 <h3>Các loài chim khác</h3>
-                <Carousel />
+                <Carousel className="Bird"/>
             </div>
         </Fragment>
     );
