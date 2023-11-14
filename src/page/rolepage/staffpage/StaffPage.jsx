62f826818db53f6574f9db56bd2e51bc7a6eb7f8
Kaminich <caominhnguyen03@gmail.com>
2023-11-14 22:35:18 +0700

    fix css and add action to viewOrderPage

--- src/page/rolepage/staffpage/StaffPage.jsx
+++ src/page/rolepage/staffpage/StaffPage.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import "../RolePage.css";
 import { Link } from "react-router-dom";
+import useAuth from "../../../hooks/useAuth";
 
 const StaffPage = () => {
-
+  const { auth } = useAuth();
   return (
     <div className="role-page">
-      <h1 className="role-page-welcome">Xin chào Tên của quản lí</h1>
+      <h1 className="role-page-welcome">Xin chào {auth?.user?.fullName}</h1>
       <h2 className="role-page-manage-tasks-title">
         Mời bạn chọn các tác vụ dưới đây để quản lí
       </h2>
@@ -15,14 +16,14 @@ const StaffPage = () => {
           <h3 className="role-page-manage-task-title">Quản lí bài viết</h3>
           <img src="./vet.jpg" className="role-page-manage-task-img" />
         </Link>
-        {/* <Link to="/feedback/view" className="role-page-manage-task"> */}
-        {/* <h3 className="role-page-manage-task-title">Quản lí feedback</h3> */}
-        {/* <img src="./vet.jpg" className="role-page-manage-task-img" /> */}
-        {/* </Link> */}
-        {/* <Link to="/order" className="role-page-manage-task"> */}
-        {/* <h3 className="role-page-manage-task-title">Quản lí đơn hàng</h3> */}
-        {/* <img src="./vet.jpg" className="role-page-manage-task-img" /> */}
-        {/* </Link> */}
+         <Link to="/feedback/view" className="role-page-manage-task"> 
+         <h3 className="role-page-manage-task-title">Quản lí feedback</h3> 
+         <img src="./vet.jpg" className="role-page-manage-task-img" /> 
+         </Link> 
+         <Link to="/order" className="role-page-manage-task"> 
+         <h3 className="role-page-manage-task-title">Quản lí đơn hàng</h3> 
+         <img src="./vet.jpg" className="role-page-manage-task-img" /> 
+         </Link> 
       </div>
     </div>
   );
