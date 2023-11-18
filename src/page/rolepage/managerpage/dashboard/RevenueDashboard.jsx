427396b0f5ccf0aea5d0a8511802eb5d00c81ebf
HuyBoon2604 <nguyengiahuy2604@gmail.com>
2023-11-18 21:33:48 +0700

    Function Total Revenue

--- src/page/rolepage/managerpage/dashboard/RevenueDashboard.jsx
+++ src/page/rolepage/managerpage/dashboard/RevenueDashboard.jsx
@@ -4,85 +4,110 @@ import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Resp
     from 'recharts';
 import { FaBox, FaMoneyBillWave, FaShoppingCart, FaUsers } from 'react-icons/fa';
 
-function RevenueDashboard() {
-    const data = [
-        {
-            name: '1',
-            revenue: 500,
-            expense: 200,
-            income: 300,
-        },
-        {
-            name: '2',
-            revenue: 500,
-            expense: 138,
-            income: 200,
-        },
-        {
-            name: '3',
-            revenue: 600,
-            expense: 180,
-            income: 500,
-        },
-        {
-            name: '4',
-            revenue: 600,
-            expense: 398,
-            income: 200,
-        },
-        {
-            name: '5',
-            revenue: 500,
-            expense: 480,
-            income: 20,
-        },
-        {
-            name: '6',
-            revenue: 100,
-            expense: 300,
-            income: 200,
-        },
-        {
-            name: '7',
-            revenue: 100,
-            expense: 430,
-            income: 200,
-        },
-        {
-            name: '8',
-            revenue: 100,
-            expense: 400,
-            income: 300,
-        },
-        {
-            name: '9',
-            revenue: 100,
-            expense: 430,
-            income: 240,
-        },
-        {
-            name: '10',
-            revenue: 100,
-            expense: 300,
-            income: 206,
-        },
-        {
-            name: '11',
-            revenue: 100,
-            expense: 400,
-            income: 208,
-        },
-        {
-            name: '12',
-            revenue: 100,
-            expense: 300,
-            income: 900,
-        }
-    ];
+function RevenueDashboard({ data }) {
+    // const data = [
+    //     {
+    //         name: '1',
+    //         revenue: 500,
+    //         expense: 200,
+    //         income: 300,
+    //     },
+    //     {
+    //         name: '2',
+    //         revenue: 500,
+    //         expense: 138,
+    //         income: 200,
+    //     },
+    //     {
+    //         name: '3',
+    //         revenue: 600,
+    //         expense: 180,
+    //         income: 500,
+    //     },
+    //     {
+    //         name: '4',
+    //         revenue: 600,
+    //         expense: 398,
+    //         income: 200,
+    //     },
+    //     {
+    //         name: '5',
+    //         revenue: 500,
+    //         expense: 480,
+    //         income: 20,
+    //     },
+    //     {
+    //         name: '6',
+    //         revenue: 100,
+    //         expense: 300,
+    //         income: 200,
+    //     },
+    //     {
+    //         name: '7',
+    //         revenue: 100,
+    //         expense: 430,
+    //         income: 200,
+    //     },
+    //     {
+    //         name: '8',
+    //         revenue: 100,
+    //         expense: 400,
+    //         income: 300,
+    //     },
+    //     {
+    //         name: '9',
+    //         revenue: 100,
+    //         expense: 430,
+    //         income: 240,
+    //     },
+    //     {
+    //         name: '10',
+    //         revenue: 100,
+    //         expense: 300,
+    //         income: 206,
+    //     },
+    //     {
+    //         name: '11',
+    //         revenue: 100,
+    //         expense: 400,
+    //         income: 208,
+    //     },
+    //     {
+    //         name: '12',
+    //         revenue: 100,
+    //         expense: 300,
+    //         income: 900,
+    //     }
+    // ];
 
+    // format tien nheeee :>
     function formatCash(currency) {
         return currency?.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ".");
     }
+    // Tong doanh thu nèee( là tổng tiền các đơn hàng đã bán dc á) :>
+    function calculateTotalRevenue(data) {
+        return data.reduce((total, item) => total + item.revenue, 0);
+    }
+    // Tong Chi Phi nèee( là tổng tiền chi phí hao hụt các đơn hàng đã bán dc á. < ĐỂ 45% phí đỡ > ) :>
+    function calculateTotalExpenses(data) {
+        const expensePercentage = 0.45; // 45%
+        const totalRevenue = calculateTotalRevenue(data);
+        const totalExpenses = totalRevenue * expensePercentage;
+
+        return totalExpenses;
+    }
+    // Tong Lợi nhuận nèee( là tổng tiền lời cuối cuùng sau khhi lấy doanh thu - chi phí ) :>
+    function calculateTotalProfit(data) {
+        const totalRevenue = calculateTotalRevenue(data);
+        const totalExpenses = calculateTotalExpenses(data);
+        const totalProfit = totalRevenue - totalExpenses;
+
+        return totalProfit;
+    }
+    // đặt biến r gọi ở dưới class thui chứ ko có zi :>
+    const totalRevenue = calculateTotalRevenue(data);
+    const totalExpenses = calculateTotalExpenses(data);
+    const totalProfit = calculateTotalProfit(data);
 
     return (
         <div className='dashboard'>
@@ -94,21 +119,21 @@ function RevenueDashboard() {
                             <h3>TỔNG DOANH THU</h3>
                             <FaMoneyBillWave className='card_icon' />
                         </div>
-                        <h1>₫{formatCash(3000000)}</h1>
+                        <h1>₫{formatCash(totalRevenue)}</h1>
                     </div>
                     <div className='card'>
                         <div className='card-inner'>
                             <h3>TỔNG CHI PHÍ</h3>
                             <FaMoneyBillWave className='card_icon' />
                         </div>
-                        <h1>₫{formatCash(3000000)}</h1>
+                        <h1>₫{formatCash(totalExpenses)}</h1>
                     </div>
                     <div className='card'>
                         <div className='card-inner'>
                             <h3>TỔNG LỢI NHUẬN</h3>
                             <FaMoneyBillWave className='card_icon' />
                         </div>
-                        <h1>₫{formatCash(3000000)}</h1>
+                        <h1>₫{formatCash(totalProfit)}</h1>
                     </div>
                 </div>
 
