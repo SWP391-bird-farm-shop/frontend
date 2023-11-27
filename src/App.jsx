7956b6e7c4350f80ac90a99adfc0669282f67e24
Kaminich <caominhnguyen03@gmail.com>
2023-11-27 17:18:57 +0700

    update feedback and productdescription

--- src/App.jsx
+++ src/App.jsx
@@ -50,13 +50,6 @@ import SearchPage from "./page/SearchPage";
 const App = () => {
   return (
     <Routes>
-      <Route element={<RoleLayout />}>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/dashboard/revenue" element={<RevenueDashboard />} />
-        <Route path="/dashboard/inventory" element={<InventoryDashboard />} />
-        <Route path="/dashboard/order" element={<OrderDashboard />} />
-      </Route>
-
       <Route path="/log-in" element={<LogInPage />} />
       <Route path="/question/:action" element={<QuestionPage />} />
       <Route path="/reset-pass" element={<ResetPasswordPage />} />
@@ -70,77 +63,29 @@ const App = () => {
         <Route path="/search/:keyword" element={<SearchPage />} />
         <Route path="/products/cages" element={<CagePage />} />
         <Route path="/products/food" element={<FoodPage />} />
-        <Route
-          path="/products/accessories-toys"
-          element={<AccessoriesToysPage />}
-        />
+        <Route path="/products/accessories-toys" element={<AccessoriesToysPage />} />
         <Route path="/blogs" element={<BlogPage />} />
         <Route path="/blog-content/:blogId" element={<BlogContentPage />} />
         <Route path="/bird/:birdId" element={<SpeciesPage />} />
         <Route path="/item-info/:productId" element={<ItemInformation />} />
         <Route path="/about-us" element={<AboutPage />} />
         <Route path="/term-condition" element={<TermsAndConditionsPage />} />
-        <Route element={<RoleLayout />}>
-          <Route
-            path="/create-voucher/:action"
-            element={<CreateVoucherPage />}
-          />
-          <Route path="/add-product/:action" element={<AddProductPage />} />
-          <Route
-            path="/update-product/:action/:productId"
-            element={<AddProductPage />}
-          />
-          <Route path="/product/:action" element={<ProductPage />} />
-          <Route path="/voucher/:action" element={<VoucherPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/dashboard/revenue" element={<RevenueDashboard />} />
-          <Route path="/dashboard/inventory" element={<InventoryDashboard />} />
-          <Route path="/dashboard/order" element={<OrderDashboard />} />
-        </Route>
       </Route>
 
       {/* user routes */}
       <Route element={<RequireAuth allowedRoles={["4"]} />}>
         <Route element={<Layout />}>
           <Route path="/user-page" element={<UserPage />} />
-          <Route path="/products/cages" element={<CagePage />} />
-          <Route path="/products/food" element={<FoodPage />} />
-          <Route
-            path="/products/accessories-toys"
-            element={<AccessoriesToysPage />}
-          />
-          <Route path="/blogs" element={<BlogPage />} />
-          <Route path="/bird/:birdId" element={<SpeciesPage />} />
-          <Route path="/item-info/:productId" element={<ItemInformation />} />
-          <Route path="/about-us" element={<AboutPage />} />
           <Route path="/cart" element={<CartPage />} />
           <Route path="/custom-cage" element={<CustomPage />} />
-          <Route
-            path="/custom-products-shape/:productId?"
-            element={<ShapePage />}
-          />
-          <Route
-            path="/custom-products-size/:productId?"
-            element={<SizePage />}
-          />
-          <Route
-            path="/custom-products-material/:productId?"
-            element={<MaterialPage />}
-          />
-          <Route
-            path="/custom-products-color/:productId?"
-            element={<ColorPage />}
-          />
-          <Route
-            path="/custom-products-end/:orderId?/:productId?"
-            element={<TotalPage />}
-          />
-
+          <Route path="/custom-products-shape/:productId?" element={<ShapePage />} />
+          <Route path="/custom-products-size/:productId?" element={<SizePage />} />
+          <Route path="/custom-products-material/:productId?" element={<MaterialPage />} />
+          <Route path="/custom-products-color/:productId?" element={<ColorPage />} />
+          <Route path="/custom-products-end/:orderId?/:productId?" element={<TotalPage />} />
           <Route path="/order-confirm" element={<ConfirmPage />} />
         </Route>
-        <Route
-          path="/update-info/:userId"
-          element={<SettingInformationPage />}
+        <Route path="/update-info/:userId" element={<SettingInformationPage />}
         />
       </Route>
 
@@ -150,10 +95,7 @@ const App = () => {
         <Route path="/admin-page" element={<AdminPage />} />
         <Route element={<RoleLayout />}>
           <Route path="/manage-account/:action" element={<ManageAccount />} />
-          <Route
-            path="/info-setting/:action/:userId"
-            element={<SettingInformationPage />}
-          />
+          <Route path="/info-setting/:action/:userId" element={<SettingInformationPage />} />
           <Route path="/create-user" element={<CreateUser />} />
         </Route>
       </Route>
@@ -162,15 +104,9 @@ const App = () => {
       <Route element={<RequireAuth allowedRoles={["2"]} />}>
         <Route path="/manager-page" element={<ManagerPage />} />
         <Route element={<RoleLayout />}>
-          <Route
-            path="/create-voucher/:action"
-            element={<CreateVoucherPage />}
-          />
+          <Route path="/create-voucher/:action" element={<CreateVoucherPage />} />
           <Route path="/add-product/:action" element={<AddProductPage />} />
-          <Route
-            path="/update-product/:action/:productId"
-            element={<AddProductPage />}
-          />
+          <Route path="/update-product/:action/:productId" element={<AddProductPage />} />
           <Route path="/product/:action" element={<ProductPage />} />
           <Route path="/voucher/:action" element={<VoucherPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
@@ -188,7 +124,7 @@ const App = () => {
           <Route path="/manage-blogs/:action" element={<BlogPage />} />
           <Route path="/view-blog/:blogId" element={<BlogContentPage />} />
           <Route path="/feedback/:action" element={<FeedbackPage />} />
-        <Route path="/item-info/:action/:productId" element={<ItemInformation />} />
+          <Route path="/item-info/:action/:productId" element={<ItemInformation />} />
           <Route path="/order/:action" element={<ViewOrderPage />} />
         </Route>
       </Route>
